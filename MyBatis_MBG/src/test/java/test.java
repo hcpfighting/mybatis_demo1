b30399cebb8cad499833f8705cbe498d01d8b0e1
hcp <hcp1418@163.com>
2023-02-16 01:58:32 +0800

    mybatis_mbg

---
+++ MyBatis_MBG/src/test/java/test.java
@@ -0,0 +1,35 @@
+import com.atguigu.mybatis.mapper.EmpMapper;
+import com.atguigu.mybatis.pojo.Emp;
+import com.atguigu.mybatis.pojo.EmpExample;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import com.github.pagehelper.PageHelper;
+
+
+public class test {
+
+    @Test
+    public void test() {
+        try {
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+            SqlSession sqlSession = sqlSessionFactory.openSession(true);
+            EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+            PageHelper.startPage(1,4);
+            List<Emp> emps = mapper.selectByExample(null);
+            emps.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
